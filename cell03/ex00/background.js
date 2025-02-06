4fa9cc6d936480986952064eeefc52e293d15759
Belle316 <65050316@kmitl.ac.th>
2025-02-06 19:26:01 +0700

    Delete cell03/ex00/background.js

--- cell03/ex00/background.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("changeColor").addEventListener("click", function() {
-    let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = randomColor;
-});
